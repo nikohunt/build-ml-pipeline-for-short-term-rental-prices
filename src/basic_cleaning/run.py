90979eefa8405ddb122d1a36535bd70686af69a9
Nikolas Hunt <nikolashunt@protonmail.ch>
2022-11-21 16:07:46 +0000

    Add out-of-bounds cleansing

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -38,6 +38,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Drop out-of-bounds datapoints
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     # Save and upload cleaned dataframe to csv
     df.to_csv(args.output_artifact, index=False)
     
